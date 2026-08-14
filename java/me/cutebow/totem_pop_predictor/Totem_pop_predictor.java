736bb2416d5b38ccb40263419fcc6f446c4a14eb
cutebow <kvlzxa@gmail.com>
2026-08-14 12:11:27 -0700

    Delete java/me/cutebow/totem_pop_predictor directory

--- java/me/cutebow/totem_pop_predictor/Totem_pop_predictor.java
+++
@@ -1,9 +0,0 @@
-package me.cutebow.totem_pop_predictor;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Totem_pop_predictor implements ModInitializer {
-    public static final String ID = "totem_pop_predictor";
-    @Override
-    public void onInitialize() { }
-}
